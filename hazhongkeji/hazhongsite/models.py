46322d6b5c3f5c806d79681ad809e7ce1a54e0f6
jiqingfen110 <313466006@qq.com>
2018-09-12 01:26:10 +0800

    2018912

--- hazhongkeji/hazhongsite/models.py
+++ hazhongkeji/hazhongsite/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class MuluOne(models.Model):
+    leibie = models.CharField(max_length=30, verbose_name='类别')
+    lianjie = models.CharField(max_length=120, verbose_name='链接')
+    title = models.CharField(max_length=240, verbose_name='标题', null=True)
+    jianjie = models.TextField(verbose_name='标题', null=True)
+
+
+class MuluTwo(models.Model):
+    leibie = models.CharField(max_length=30, verbose_name='类别')
+    lianjie = models.CharField(max_length=120, verbose_name='链接')
+    title = models.CharField(max_length=240, verbose_name='标题', null=True)
+    jianjie = models.TextField(verbose_name='标题', null=True)
+    wijian_one = models.ForeignKey(to='MuluOne',on_delete=models.CASCADE)
+
