e8ea9467b27b387cab35d7ca61cc694d5ed29292
jiqingfen110 <313466006@qq.com>
2018-10-17 00:06:48 +0800

    20181017

--- hazhongkeji/hazhongsite/urls.py
+++ hazhongkeji/hazhongsite/urls.py
@@ -5,10 +5,4 @@ from hazhongsite import views
 
 urlpatterns = [
     url(r'^$',views.HazhongSite.as_view()),
-    # url(r'^$', views.index),
-    # url(r'^index', views.index),
-    # url(r'^about', views.about),
-    # url(r'^', views.index),
-    # url(r'^', views.index),
-    # url(r'^', views.index),
 ]
--- hazhongkeji/hazhongsite/views.py
+++ hazhongkeji/hazhongsite/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render, HttpResponse
+from django.core import serializers
+from django.http import JsonResponse
 from rest_framework.views import APIView
-
-from rest_framework.authentication import BaseAuthentication
-
-
-class MyAuth(BaseAuthentication):
-    def authenticator(self,request):
-        pass
-
+from . import models
 
 
 class HazhongSite(APIView):
-    authentication_classes = [MyAuth,]
+    def get(self,request,*args,**kwargs):
+        context = {}
+        s = models.MuluOne.objects.all().first()
+        print(s,'**********',type(s))
+        context['title'] = s
+        # print(context)
+        # cc = serializers.serialize('json',context)
+        return HttpResponse('hello') #JsonResponse(context,)
 
-    def get(self):
-        self.dispatch
-        pass
-    
+
+
+
+
+
+
+
+
+
 # def index(request):
 #
 #     return render(request, 'index.html')
