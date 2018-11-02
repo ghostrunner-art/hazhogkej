1d140f1cd20a16adddc2d7f046a8fc8628d386cb
jiqingfen110 <313466006@qq.com>
2018-11-02 16:38:09 +0800

    2018.11.02

--- hazhongkeji/zhizhu_proxy/forms.py
+++ hazhongkeji/zhizhu_proxy/forms.py
@@ -5,13 +5,34 @@ from . import models
 
 class Myforms(forms.Form):
     cc = models.City.objects.all()
-    username = forms.CharField(label='姓名',max_length=32)
+    username = forms.CharField(
+        label='姓名',
+        max_length=32,
+        error_messages={
+            'required': '姓名不能为空',
+            'invalid': '格式错误',
+        },
+    )
     phone = forms.CharField(
         label='联系方式',
         max_length=11,
         min_length=11,
+        error_messages={
+            'required':'电话号不能为空',
+            'invalid':'格式错误',
+            'max_length':'电话号必须为11位',
+            'min_length':'电话号必须为11位',
+        },
     )
-    number = forms.CharField(label='网吧代码',max_length=32)
-    choice = forms.ModelChoiceField(label='地区选择',queryset=cc,empty_label='选择地区',)
+    # number = forms.CharField(label='网吧代码',max_length=32)
+    choice = forms.ModelChoiceField(
+        label='地区选择',
+        queryset=cc,
+        empty_label='选择地区',
+        error_messages={
+            'required': '地区为必选项',
+            'invalid': '格式错误',
+        },
+    )
 
 
--- hazhongkeji/zhizhu_proxy/views.py
+++ hazhongkeji/zhizhu_proxy/views.py
@@ -3,54 +3,36 @@ from django.shortcuts import render
 from . import models
 from .forms import Myforms
 
-
 class Login(View):
     def get(self, request, *args, **kwargs):
         form = Myforms()
         return render(request, 'login-forms.html', {'form': form})
 
-
-    def post(self, request):
-        form = Myforms(request.POST)
+    def post(self, request, *args, **kwargs):
+        form = Myforms(request.POST)  # 请求数据做参数
         if form.is_valid():
             centent = {}
 
-            phone_valid =form.cleaned_data['phone'] #推荐这么取验证后的数据
-            phone = request.POST['phone'] #取原始数据
-            centent['phone_valid'] =phone_valid
-            centent['phone'] =phone
+            phone_valid = form.cleaned_data['phone']  # 推荐这么取验证后的数据
+            phone = request.POST['phone']  # 取原始数据
+            centent['phone_valid'] = phone_valid
+            centent['phone'] = phone
 
-            print(phone_valid,phone)
-            return render(request,'index.html',centent)
-
-
-# class Login(View):
-#     def get(self, request, *args, **kwargs):
-#         return render(request, 'login.html')
-#
-#     def post(self, request):
-#         form = Myforms(request.POST)
-#
-#         centent = {}
-#         phone = request.POST['phone']
-#         username = request.POST['username']
-#         number = request.POST['number']
-#         models.UserInfo.objects.create(username=username, phone=phone, number=number)
-#
-#         centent['username'] = username
-#         centent['phone'] = phone
-#         centent['number'] = number
-#         print('ok')
-#         return render(request, 'index.html', centent)
+            print(phone_valid, phone)
+            return render(request, 'index.html', centent)
+        else:
+            error = {}
+            error['form'] = form
+            error['error'] = form.errors
+            return render(request, 'login-forms.html', error)
 
 
 class zhizhu_index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
 
+
 class Shuju(View):
     def get(self, request, num):
         print(num)
         return render(request, 'error.html')
-
-
