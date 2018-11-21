93cab0f3fb178a3804b10c9d35d3c5135946a327
jiqingfen110 <313466006@qq.com>
2018-11-21 10:50:14 +0800

    2018.11.21.01

--- hazhongkeji/hazhongkeji/settings.py
+++ hazhongkeji/hazhongkeji/settings.py
@@ -77,16 +77,14 @@ WSGI_APPLICATION = 'hazhongkeji.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'zhizhu',
-    #     'USER': 'zhizhu',
-    #     'PASSWORD': '123456',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    # },
-
-
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zhizhu',
+        'USER': 'zhizhu',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
 }
 
 
